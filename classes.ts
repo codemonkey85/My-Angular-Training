2b2fee94c5e1320be819878ded93f8a25cceaf59
Michael Bond <codemonkey85@gmail.com>
2018-09-04 13:01:35 -0400

    More testing / training

---
+++ classes.ts
@@ -0,0 +1,50 @@
+class Product {
+    constructor(
+        private _title: string,
+        private _price: number
+    ) {}
+    get Title(): string {
+        return this._title;
+    }
+    set Title(value: string) {
+        this._title = value;
+    }
+    describe() {
+        return `Product ${this._title} is priced ${this._price} $`;
+    }
+}
+
+class OnlineProduct extends Product {
+    constructor(
+        protected _title: string,
+        protected _price: number,
+        private deliveryMode: string
+    ){
+        super(_title, _price);
+    }
+    describe() {
+        return `Product ${this._title} is priced ${this._price} $`;
+    }
+}
+
+const p10 = new Product("MacBook Pro", 2000);
+p10.describe();
+p10.Title = "Dell XPS";
+
+interface IMediaPlayer{
+    play: () => void;
+    pause: () => void;
+    volume: (value: number) => boolean;
+}
+
+class MediaPlayer implements IMediaPlayer{
+    play(){
+        
+    }
+    pause(){
+        
+    }
+    volume(value: number){
+        return true;
+    }
+}
--- function.ts
+++ function.ts
@@ -22,3 +22,35 @@ function makeName1(first: string, last: string, title:string = "Mr"): string {
 let fullName1 = makeName1("Michael", "Bond");
 
 console.log(fullName1);
+
+// es6 Arrow functions
+const makeName2 = (
+    first: string,
+    last: string,
+    title: string = "mr"
+): string => {
+    if(title !== undefined){
+        return title + ". " + first + " " + last;
+    }
+    return first + " " + last;
+}
+
+let numbers1: Array<number> = [10, 20, 30, 40, 50];
+numbers1.filter(function(n){
+    return n > 30;
+})
+
+numbers1.filter(n => n > 30);
+
+// this is an auto bind
+function Person(name1: string, age: number){
+    this.name1 = name;
+    this.age = age;
+    this.describe = () => {
+        return `Name: ${this.name1} Age: ${this.age}`;
+    };
+}
+
+var p1 = new Person("test", 32);
+var describeRef = p1.describe;
+console.log(describeRef());
